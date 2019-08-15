7b7ffde7ccc75b4ef6b104a03bbc899d3208f672
gulang <876427869@qq.com>
2019-08-15 09:15:41 +0800

    add base class

---
+++ src/com/siqi_dangjian/dao/IUserDao.java
@@ -0,0 +1,17 @@
+package com.siqi_dangjian.dao;
+
+import com.siqi_dangjian.bean.User;
+
+import java.util.List;
+import java.util.Map;
+
+public interface IUserDao {
+
+    void inserOrUpdateUser(User user) throws Exception;
+
+    void logicDeleteUser(User user) throws Exception;
+
+    User selectUserById(Long id) throws Exception;
+
+    List<User> selectAllUser(Map blurParam, Map dateParam, Map intParam, int limit, int page) throws Exception;
+}
---
+++ src/com/siqi_dangjian/dao/impl/UserDao.java
@@ -0,0 +1,33 @@
+package com.siqi_dangjian.dao.impl;
+
+import com.siqi_dangjian.bean.User;
+import com.siqi_dangjian.dao.IUserDao;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class UserDao extends BaseDao<User> implements IUserDao {
+
+
+    @Override
+    public void inserOrUpdateUser(User user) throws Exception {
+        saveOrUpdateObject(user);
+    }
+
+    @Override
+    public void logicDeleteUser(User user) throws Exception {
+        logic_delete(user);
+    }
+
+    @Override
+    public User selectUserById(Long id) throws Exception {
+        return getObjectById(id);
+    }
+
+    @Override
+    public List<User> selectAllUser(Map blurParam, Map dateParam, Map intParam, int limit, int page) throws Exception {
+        return getAll();
+    }
+}
